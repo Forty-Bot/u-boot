6e16f18dd316d831368f43063df73e7bda95a4a5
Sean Anderson <seanga2@gmail.com>
2019-12-17 20:05:23 -0500

    riscv: Add initial support for the Sipeed Maix
    Signed-off-by: Sean Anderson <seanga2@gmail.com>

---
+++ board/sipeed/maix/maix.c
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2019 Sean Anderson <seanga2@gmail.com>
+ */
+
+#include <asm/sections.h>
+#include <config.h>
+#include <fdt_support.h>
+#include <hexdump.h>
+#include <stdio.h>
+
+void *board_fdt_blob_setup(void)
+{
+	/* FIXME: the magic number gets overwritten for some reason... */
+	u32 *fdt_blob = (u32 *)_end;
+	return fdt_blob;
+}
+
+int board_init(void)
+{
+	return 0;
+}
---
+++ include/configs/sipeed-maix.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2019 Sean Anderson <seanga2@gmail.com>
+ */
+
+
+#ifndef CONFIGS_SIPEED_MAIX_H
+#define CONFIGS_SIPEED_MAIX_H
+
+#include <linux/sizes.h>
+
+#define CONFIG_SYS_LOAD_ADDR 0x80000000
+#define CONFIG_SYS_INIT_SP_ADDR 0x805FFFFF
+#define CONFIG_SYS_MALLOC_LEN SZ_8M
+#define CONFIG_SYS_CACHELINE_SIZE 32
+
+#endif /* CONFIGS_SIPEED_MAIX_H */

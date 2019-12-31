8dac6565445535e56e719c9ef47ed93804fe6034
Sean Anderson <seanga2@gmail.com>
2019-12-31 14:28:58 -0500

    riscv: Add device tree for K210
    The cache-line size is undocumented. Emphirical tests suggest that it is 32
    bytes, but I've used 64-bytes to be on the safe side.
    
    Where possible, I have tried to find compatible drivers based on the layout
    of registers. However, I have not tested most of this functionality, and
    most devices should be considered descriptive at best. I would appreciate
    if anyone could help identify possibly compatible devices, especially for
    the timers, watchdogs, and rtc.
    
    Signed-off-by: Sean Anderson <seanga2@gmail.com>
    Series-changes: 2
    - Model changed to "Sipeed Maix Bit" to match file name
    - Value of stdout-path fixed
    - SD card slot compatible changed to "mmc-spi-slot"
    - "jedec,spi-nor" added to spi flash compatible list
    - Aliases for spi busses added
    - timebase-frequency divided by 50 to match timer speed
    - cpu-frequency renamed to clock-frequency
    - CPUX_intc restyled to cpuX_intc
    - "kendryte,k210-soc" added to soc compatible list for future-proofing
    - PLIC handle renamed to plic0 from pic0
    - K210_RST_SOC removed from sysrst, due to not being located in the reset
      register
    - K210_RST_* numbers changed to match their bit offset within the reset
      register
    - gpio_controller restyled to gpio-controller
    - Added a second clock to the dma binding to match what the driver expects
    - Changed "snps,designware-spi" compatible string to "snps,dw-apb-ssi" to
      match the correct driver
    - Added a name to the spi clocks
    - Added reg-io-width property to spi bindings
    - Assigned a default parent to K210_CLK_SPI3
    - Removed assigned clocks for ACLK and PLLs
    - Removed u-boot,dm-pre-reloc bindings
    
    Series-changes: 3
    - Move this patch to the end of the series
    - Add a max frequency for spi3
    - Remov unused compatible strings from spi-flash@0
    - Add s and u to isa string
    - Fix mmu-type
    - Remove cache-line size since it is unused (in u-boot) and undocumented
      (upstream)
    - Add timer interrupts to clint0
    - Round up various registers
    - Add riscv,max-priority to plic
    - Add apb* busses, since they have clocks which need to be enabled to
      access their devices
    - Change uart compatible strings to "snps,dw-apb-uart", since that appears
      to match their registers
    - Add compatible string for wdt*
    - Add system reset device under sysctl
    - Add reset device under sysctl
    
    Series-changes: 4
    - Set regs sizes to full address range
    - Remove clock-frequency property from cpus
    - Add spi-max-frequency to spi devices from documentation
    - Add more compatible strings for each device
    - Add AI ram as a separate memory bank. Its clock is disabled on boot, and
      it cannot be accessed
    - Reorder memory banks so u-boot relocates higher, leaving more room to
      load boot images
    - Add designware ssi CTRL0 field shifts to spi devices
    - Don't enable the MMC slot
    - Update copyright
    - Lint

--- include/dt-bindings/clock/k210-sysctl.h
+++ include/dt-bindings/clock/k210-sysctl.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
- * Copyright (C) 2019 Sean Anderson <seanga2@gmail.com>
+ * Copyright (C) 2019-20 Sean Anderson <seanga2@gmail.com>
  */
 
 #ifndef CLOCK_K210_SYSCTL_H

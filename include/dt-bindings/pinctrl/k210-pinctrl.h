d2a40843b3f5853467781c21192ce0c2f707cb82
Sean Anderson <seanga2@gmail.com>
2020-02-15 13:37:46 -0500

    pinctrl: Add support for Kendryte K210 FPIOA
    The Fully-Programmable Input/Output Array (FPIOA) device controls pin
    multiplexing on the K210. The FPIOA can remap any supported function to any
    multifunctional IO pin. It can also perform basic GPIO functions, such as
    reading the current value of a pin.
    
    Signed-off-by: Sean Anderson <seanga2@gmail.com>
    Commit-notes:
    This patch was previously submitted as part of
    https://patchwork.ozlabs.org/project/uboot/list/?series=161576
    
    Changes from that version include:
    - Reformat to reduce errors from checkpatch
    END

---
+++ include/dt-bindings/pinctrl/k210-pinctrl.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2020 Sean Anderson <seanga2@gmail.com>
+ */
+
+#ifndef DT_K210_PINCTRL_H
+#define DT_K210_PINCTRL_H
+
+#define K210_PC_POWER_3V3 0
+#define K210_PC_POWER_1V8 1
+
+#endif /* DT_K210_PINCTRL_H */

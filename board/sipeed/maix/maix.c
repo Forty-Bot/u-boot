8eb047811771ef35e132af00ce3920bbfc61cc3e
Sean Anderson <seanga2@gmail.com>
2020-09-28 22:36:43 -0400

    riscv: Enable AI ram on K210
    CONFIG_SYS_SDRAM_{BASE,SIZE} are no longer necessary. The default
    get_effective_memsize will also work. The clock driver creates a bunch of
    devices, so we need to increase the pre-reloc malloc arena.
    
    Signed-off-by: Sean Anderson <seanga2@gmail.com>

--- board/sipeed/maix/maix.c
+++ board/sipeed/maix/maix.c
@@ -9,33 +9,7 @@
 #include <fdt_support.h>
 #include <asm/io.h>
 
-phys_size_t get_effective_memsize(void)
-{
-	return CONFIG_SYS_SDRAM_SIZE;
-}
-
 int board_init(void)
 {
-	int ret, i;
-	const char * const banks[] = { "sram0", "sram1", "airam" };
-	ofnode memory;
-	struct clk clk;
-
-	/* Enable RAM clocks */
-	memory = ofnode_by_compatible(ofnode_null(), "kendryte,k210-sram");
-	if (ofnode_equal(memory, ofnode_null()))
-		return -ENOENT;
-
-	for (i = 0; i < ARRAY_SIZE(banks); i++) {
-		ret = clk_get_by_name_nodev(memory, banks[i], &clk);
-		if (ret)
-			continue;
-
-		ret = clk_enable(&clk);
-		clk_free(&clk);
-		if (ret)
-			return ret;
-	}
-
 	return 0;
 }
